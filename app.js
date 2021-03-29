888d818c668b83ba96f985ca1b54f9d560b200a8
aniltuncay <aniltuncay34@gmail.co>
2021-03-30 01:46:38 +0200

    edited

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const openMenu = document.getElementById("openMenu");
 const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
 
-function mediaQueries(x) {
-    if (x.matches) { 
+console.log(menu)
+
+function mediaQueries(condition) {
+    if (condition.matches) { 
         openMenu.style.display = "none";
+        menu.style.display="none";
+        hamburger.style.display="flex"
+        hamburger.style.justifyContent = "center";
     } else {
         openMenu.style.display = "none";
+        menu.style.display="block";
     }
   }
   
-  let x = window.matchMedia("(max-width: 600px)")
-  mediaQueries(x) 
-  x.addListener(mediaQueries) 
+  let condition = window.matchMedia("(max-width: 600px)")
+  mediaQueries(condition) 
+  condition.addListener(mediaQueries) 
 
 hamburger.addEventListener("click", function () {
 
     if (openMenu.style.display === "block") {
-        console.log("ilk if.")
         openMenu.style.display = "none";
     }
     else {
-
-        console.log("2. if.")
         openMenu.style.display = "block"
     }
 
